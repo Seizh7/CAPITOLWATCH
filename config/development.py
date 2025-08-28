a213287b1d3aeb9c9b0236b3b4b02b94e998832e
Seizh7 <social_seizh@protonmail.com>
2025-08-28 21:56:18 +0200

    Add new config structure with development and production modules

---
+++ config/development.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2025 Seizh7
+# Licensed under the Apache License, Version 2.0
+# (http://www.apache.org/licenses/LICENSE-2.0)
+
+# config/development.py
+from pathlib import Path
+import os
+
+
+class Config:
+    def __init__(self, year="2023"):
+        self.project_root = Path(__file__).resolve().parent.parent
+
+        self.data_dir = self.project_root / "data"
+        self.source_dir = self.project_root / "capitolwatch"
+
+        self.datapipeline_dir = self.source_dir / "datapipeline"
+        self.scraping_dir = self.datapipeline_dir / "scraping"
+        self.parsing_dir = self.datapipeline_dir / "parsing"
+        self.database_dir = self.datapipeline_dir / "database"
+
+        self.year = year
+        self.search_year = str(int(self.year) + 1)
+        self.output_folder = self.data_dir / f"annual_reports_{self.year}"
+        self.start_date = f"01/01/{self.search_year}"
+        self.end_date = f"12/31/{self.search_year}"
+
+        self.base_url = "https://efdsearch.senate.gov"
+        self.search_url = self.base_url + "/search/"
+
+        self.db_path = self.data_dir / "capitolwatch_dev.db"
+
+        self.api_key = os.getenv("CONGRESS_API_KEY")
+        if not self.api_key:
+            raise ValueError("Missing CONGRESS_API_KEY (development)")
+
+        self.debug = True
